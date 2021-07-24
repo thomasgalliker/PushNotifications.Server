7020ea577a62e2c4e10c06aab540a62be16b7726
Thomas Galliker <thomas@bluewin.ch>
2021-07-24 18:47:33 +0200

    Add more sample code to console project

--- Samples/PushNotifications.ConsoleSample/Program.cs
+++ Samples/PushNotifications.ConsoleSample/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using PushNotifications.Apple;
 using PushNotifications.ConsoleSample.Logging;
+using PushNotifications.Google;
 using PushNotifications.Logging;
 
 namespace PushNotifications.ConsoleSample
@@ -14,10 +17,7 @@ namespace PushNotifications.ConsoleSample
         private static readonly PushDevice[] pushDevices = new[]
           {
             PushDevice.Android("dBpr37I3WlI:APA91bHqhmzZVoUd2hE9Yw-s3wDOtzexg0LkDew59q0Q1hjc2a3KN0kZu0fSZpqSIej346F69q0eKm3u0WJEgG3_AOM44E3DH-AvnHM6vjIMRora-eXKyJ7kDZ5F1lpZXfNb1B0hxmeS"),
-            PushDevice.Android("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
-            PushDevice.iOS("85bea18076def67319aa2345e30ca5fbce20296e2af05640cd6036c9543dbbb3"), // Token expired
-            PushDevice.iOS("235857441ce4ad2fa491c48738dafb1e456cf5d76252967bd4ceb5a4ccb11777"), // Valid Token
-            PushDevice.iOS("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"),
+            PushDevice.iOS("235857441ce4ad2fa491c48738dafb1e456cf5d76252967bd4ceb5a4ccb11777"),
         };
 
         static void Main(string[] args)
@@ -35,32 +35,101 @@ namespace PushNotifications.ConsoleSample
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
-            var configurationSection = config.GetSection("PushNotification:iOS");
-            var options = new ApnsJwtOptions();
-            configurationSection.Bind(options);
+            var sectionApnsJwtOptions = config.GetSection("PushNotifications:ApnsJwtOptions");
+            var apnsJwtOptions = new ApnsJwtOptions();
+            sectionApnsJwtOptions.Bind(apnsJwtOptions);
 
-            SendApnsPushNotification(options, "85bea18076def67319aa2345e30ca5fbce20296e2af05640cd6036c9543dbbb3").Wait();
+            var sectionFcmConfiguration = config.GetSection("PushNotifications:FcmConfiguration");
+            var fcmConfiguration = new FcmConfiguration();
+            sectionFcmConfiguration.Bind(fcmConfiguration);
+
+            IApnsClient apnsClient = new ApnsClient(apnsJwtOptions);
+
+            IFcmClient fcmClient = new FcmClient(fcmConfiguration);
+
+            SendApnsPushNotification(apnsClient).Wait();
+
+            SendXPushNotifications(fcmClient, apnsClient).Wait();
 
             Console.ReadKey();
         }
 
-        private static async Task SendApnsPushNotification(ApnsJwtOptions options, string token)
+        private static async Task<PushResponse> SendXPushNotifications(IFcmClient fcmClient, IApnsClient apnsClient)
         {
-            IApnsClient apnsClient = new ApnsClient(new ConsoleLogger(), new HttpClient(), options);
+            var pushNotificationClient = new PushNotificationClient(new ConsoleLogger(), fcmClient, apnsClient);
 
-            var push = new ApnsRequest(ApplePushType.Alert)
-                .AddToken(token)
-                .AddAlert("Test Message", $"Message from PushNotifications.ConsoleSample @ {DateTime.Now}")
-                .AddCustomProperty("key", "value");
-
-            var response = await apnsClient.SendAsync(push);
-            if (response.IsSuccessful)
+            var pushRequest = new PushRequest
             {
-                Console.WriteLine($"Successfully sent push notification to device {token}");
+                Content = new PushContent
+                {
+                    Title = "Test Message",
+                    Body = $"Message from PushNotifications.AspNetCoreSample @ {DateTime.Now}",
+                    CustomData = new Dictionary<string, string>
+                    {
+                        { "key", "value" }
+                    }
+                },
+                Devices = pushDevices
+            };
+
+            var pushResponse = await pushNotificationClient.SendAsync(pushRequest);
+
+            if (pushResponse.IsSuccessful)
+            {
+                Console.WriteLine($"Successfully sent push notification to {pushResponse.Results.Count} devices");
             }
             else
             {
-                Console.WriteLine($"Failed to send push notification to device {token}: {response.Reason}");
+                // Detect all push device tokens which are expired, not registered, etc...
+                // These tokens need to be removed from our push notification repository/database.
+                var tokensWithRegistrationProblem = pushResponse.GetTokensWithRegistrationProblem();
+                if (tokensWithRegistrationProblem.Any())
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(
+                        $"Following device tokens have registration problems:{Environment.NewLine}" +
+                        $"{string.Join(Environment.NewLine, tokensWithRegistrationProblem)}");
+                }
+
+                Console.WriteLine();
+                // PushResponse not only consolidates the received push responses
+                // it also allows to evaluate the individual results (in case of a multicast request).
+                foreach (var result in pushResponse.Results)
+                {
+                    if (result.IsSuccessful)
+                    {
+                        Console.WriteLine($"Successfully sent push notification to DeviceToken={result.DeviceToken}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to send push notification to DeviceToken={result.DeviceToken}"); // TODO: Log reason for error here!
+                    }
+                }
+            }
+
+            return pushResponse;
+        }
+
+        private static async Task SendApnsPushNotification(IApnsClient apnsClient)
+        {
+            foreach (var pushDevice in pushDevices.Where(d => d.Platform == RuntimePlatform.iOS))
+            {
+                var token = pushDevice.DeviceToken;
+
+                var apnsRequest = new ApnsRequest(ApplePushType.Alert)
+                    .AddToken(pushDevice.DeviceToken)
+                    .AddAlert("Test Message", $"Message from PushNotifications.ConsoleSample @ {DateTime.Now}")
+                    .AddCustomProperty("key", "value");
+
+                var apnsResponse = await apnsClient.SendAsync(apnsRequest);
+                if (apnsResponse.IsSuccessful)
+                {
+                    Console.WriteLine($"Successfully sent push notification to device {token}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to send push notification to device {token}: {apnsResponse.Reason}");
+                }
             }
         }
 
