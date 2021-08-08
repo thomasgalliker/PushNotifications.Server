df8d0c28dd6e5a8e6918953cffd04e897fee813d
Thomas Galliker <thomas@bluewin.ch>
2021-08-08 09:36:55 +0200

    Split program into partial class -> Fcm / FcmLegacy / Apns

---
+++ Samples/PushNotifications.ConsoleSample/Program.Fcm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using PushNotifications.ConsoleSample.Demodata;
+using PushNotifications.Google;
+
+namespace PushNotifications.ConsoleSample
+{
+    partial class Program
+    {
+        private static async Task SendFcmPushNotification()
+        {
+            var sectionFcmOptions = configuration.GetSection("PushNotifications:FcmOptions");
+            var fcmOptions = new FcmOptions();
+            sectionFcmOptions.Bind(fcmOptions);
+
+            IFcmClient fcmClient = new FcmClient(fcmOptions);
+
+            var pushDevices = PushDevices.Get()
+               .Where(d => d.Platform == RuntimePlatform.Android)
+               .ToList();
+
+            foreach (var pushDevice in pushDevices)
+            {
+                var token = pushDevice.DeviceToken;
+
+                var fcmRequest = new FcmRequest()
+                {
+                    Message = new Message
+                    {
+                        Token = token,
+                        Notification = new Notification
+                        {
+                            Title = "Test Message",
+                            Body = $"Message from PushNotifications.ConsoleSample @ {DateTime.Now}",
+                        },
+                        Data = new Dictionary<string, string>
+                        {
+                            { "key", "value" }
+                        },
+                    },
+                    ValidateOnly = false,
+                };
+
+                var fcmResponse = await fcmClient.SendAsync(fcmRequest);
+                if (fcmResponse.IsSuccessful)
+                {
+                    Console.WriteLine($"Successfully sent push notification");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to send push notification");
+                }
+            }
+        }
+    }
+}
---
+++ Samples/PushNotifications.ConsoleSample/Program.FcmLegacy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using PushNotifications.ConsoleSample.Demodata;
+using PushNotifications.Google.Legacy;
+
+namespace PushNotifications.ConsoleSample
+{
+    partial class Program
+    {
+        private static async Task SendFcmLegacyPushNotification()
+        {
+            var sectionFcmOptions = configuration.GetSection("PushNotifications:FcmLegacyOptions");
+            var fcmOptions = new FcmOptions();
+            sectionFcmOptions.Bind(fcmOptions);
+
+            IFcmClient fcmClient = new FcmClient(fcmOptions);
+            var pushDevices = PushDevices.Get()
+                .Where(d => d.Platform == RuntimePlatform.Android)
+                .ToList();
+
+            var fcmRequest = new FcmRequest()
+            {
+                RegistrationIds = pushDevices.Select(d => d.DeviceToken).ToList(),
+                Notification = new FcmNotification
+                {
+                    Title = "Test Message",
+                    Body = $"Message from PushNotifications.ConsoleSample @ {DateTime.Now}",
+                },
+                Data = new Dictionary<string, string>
+                {
+                    { "key", "value" }
+                },
+            };
+
+            var fcmResponse = await fcmClient.SendAsync(fcmRequest);
+            if (fcmResponse.IsSuccessful)
+            {
+                Console.WriteLine($"Successfully sent push notification");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to send push notification");
+            }
+        }
+    }
+}
