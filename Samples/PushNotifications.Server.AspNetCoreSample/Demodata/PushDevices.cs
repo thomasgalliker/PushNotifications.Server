2ed943c737f7ab29509bb75c5298cc221cad18cd
Thomas Galliker <thomas@bluewin.ch>
2021-09-23 23:42:54 +0200

    Add new test devices

--- Samples/PushNotifications.Server.AspNetCoreSample/Demodata/PushDevices.cs
+++ Samples/PushNotifications.Server.AspNetCoreSample/Demodata/PushDevices.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace PushNotifications.Server.Server.AspNetCoreSample.Demodata
+namespace PushNotifications.Server.AspNetCoreSample.Demodata
 {
     public static class PushDevices
     {
@@ -9,7 +9,7 @@ namespace PushNotifications.Server.Server.AspNetCoreSample.Demodata
             yield return PushDevice.Android("ciXaQ5lURTO-IZlv35l7Yg:APA91bFN8KyBjn0PiacvbLv0vuYWGaAz42AKuP-neb5Jpzj0Rqwv9SJndi2zR-AUR00oClJmGBJc4ZjrBM4qGbEkG09ID-8-_lf8HBrTTwMPtI_EddfZi-M4fmyWFzfRdxce-2Zo1z2Q");
             yield return PushDevice.Android("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"); // Wrong token
             yield return PushDevice.iOS("85bea18076def67319aa2345e30ca5fbce20296e2af05640cd6036c9543dbbb3"); // Token expired
-            yield return PushDevice.iOS("235857441ce4ad2fa491c48738dafb1e456cf5d76252967bd4ceb5a4ccb11777"); // Valid Token
+            yield return PushDevice.iOS("4b22b7774ba3e5daf089f97e6f0c7f15262e0d4b4018ce8b00d749c1c218cbb0"); // Valid Token
             yield return PushDevice.iOS("IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"); // Wrong token
         }
     }
